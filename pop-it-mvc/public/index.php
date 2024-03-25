0a264050f1cda624ddebb6633a8e7e944a51412c
5kywalker724 <aleftinapumpur@gmail.com>
2024-03-25 13:56:25 +0700

    Создание оставшихся файлов, отвечающих за разработку стартовых скриптов

--- pop-it-mvc/public/index.php
+++ pop-it-mvc/public/index.php
@@ -1,2 +1,11 @@
 <?php
-echo "hello";
+declare(strict_types=1);
+
+try{
+    $app = require_once __DIR__ . '/../core/bootstrap.php'
+    $app->run();
+} cath (\Throwable $exception){
+    echo '<pre>';
+    print_r($exception);
+    echo '</pre>';
+}
